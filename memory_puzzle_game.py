cf6e857827e23602a47b7db1c7503cb9fc8eadc3
Devonte Rogers <93460515+JustMyLuck6@users.noreply.github.com>
2023-10-23 17:39:56 -0500

    Create memory_puzzle_game.py

---
+++ memory_puzzle_game.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import random
+from tkinter import ttk
+
+PuzzleWindow=Tk()
+
+PuzzleWindow.title('Memory Puzzle Game By DataFlair')
+
+tabs = ttk.Notebook(PuzzleWindow) 
+easy= ttk.Frame(tabs)
